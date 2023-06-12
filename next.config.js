d867b3f776e2c95afedc6f0f3124bdfbb9372caf
hicode-dev <kingdavies2021@gmail.com>
2023-06-12 13:53:40 -0700

    lol

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['easetemplate.com'],
+      },
+}
 
 module.exports = nextConfig
